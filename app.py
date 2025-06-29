18bf6dd5dd2cc0e0eed304dd5a6cfd9937dd3d8b
Ah-kin <123722750+TuskedZeus@users.noreply.github.com>
2025-06-29 07:03:46 -0600

    Update app.py
    Corrected vehicle_us route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('C:\\Users\\Ah-kin\\Python\\Project_7_Root\\Project-7\\vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Análisis de datos de vehículos')
 

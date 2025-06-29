caf3a2f5260159112bba1d02be9fc1e3e199a6aa
Ah-kin <123722750+TuskedZeus@users.noreply.github.com>
2025-06-29 07:14:23 -0600

    Update app.py
    Corrected Dataframe name.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv')
 
 st.header('Análisis de datos de vehículos')
 
